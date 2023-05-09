4bceeded40ac86d88c8bb8369ae31e9f92a7c708
Ouzx <oguzhankandakoglu@gmail.com>
2023-05-09 21:07:56 +0300

    footer added

--- dist/routes/footer.js
+++ dist/routes/footer.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const footer_1 = require("../controller/footer");
 const router = (0, express_1.Router)();
 /* ROUTES */
+router.get("/", footer_1.getFooterData);
 exports.default = router;
--- routes/footer.ts
+++ routes/footer.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { getFooterData } from "../controller/footer";
 const router = Router();
 
 /* ROUTES */
+router.get("/", getFooterData);
 
 export default router;
