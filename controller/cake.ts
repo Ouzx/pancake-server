8733b3fbff59bbdbe30b6b8b8b15213150dd765c
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-05-17 00:41:01 +0300

    probably finished

--- controller/cake.ts
+++ controller/cake.ts
@@ -1,5 +1,40 @@
 import { Request, Response } from "express";
 
+const data = {
+  headerFirstWord: "CAKE",
+  headerText: " makes our world go round.",
+  paragraphText:
+    "CAKE token is at the heart of the PancakeSwap ecosystem. Buy it, win it, farm it, spend it, stake it... heck, you can even vote with it!",
+  buttonText: "Buy CAKE",
+  linkText: "Learn",
+  blocks: [
+    {
+      entry: "Circulating Supply",
+      data: "200,369,318",
+    },
+    {
+      entry: "Total supply",
+      data: "385,464,216",
+    },
+    {
+      entry: "Max Supply",
+      data: "750,000,000",
+    },
+    {
+      entry: "Market cap",
+      data: "$370 million",
+    },
+    {
+      entry: "Burned to date",
+      data: "826,186,824",
+    },
+    {
+      entry: "Current emissions",
+      data: "5.23/block",
+    },
+  ],
+};
+
 export const getCakeData = async (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello from cake" });
+  res.status(200).json(data);
 };
--- dist/routes/startInSeconds.js
+++ dist/routes/startInSeconds.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const startInSeconds_js_1 = require("../controller/startInSeconds.js");
 const router = (0, express_1.Router)();
 /* ROUTES */
+router.get("/", startInSeconds_js_1.getStartInSecondsData);
 exports.default = router;
